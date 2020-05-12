14bd542264235bc5c174d56cc18af2b6c70bb537
Marlen Jacob <maj@riseup.net>
2020-05-12 19:44:00 +0200

    add certificates

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const fs = require("fs");
+
 const path = require("path");
 const app = express();
+const port = 443;
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/ping", function (req, res) {
@@ -12,4 +15,31 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(9000, () => console.log("Server is up"));
+var https_options = {
+  key: fs.readFileSync("certificates/privkey.pem"),
+
+  cert: fs.readFileSync("certificates/cert.pem"),
+
+  ca: [
+    fs.readFileSync("certificates/chain.pem"),
+
+    fs.readFileSync("certificates/fullchain.pem"),
+  ],
+};
+
+app.listen(port, () => console.log("Server is up"));
+
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  response.end("Hello Node.js Server with HTTPS");
+};
+
+const server = https.createServer(https_options, app);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log("something bad happened", err);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
