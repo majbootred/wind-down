5b4b73f9b1975610af3f229d550611d3ae3837ec
Marlen Jacob <maj@riseup.net>
2020-05-18 18:11:49 +0200

    add boostrap, add single components in routing

--- src/App.js
+++ src/App.js
@@ -2,81 +2,47 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Nav, Container, Row, Col } from "react-bootstrap";
+
+import Home from "./Home";
+import DBTest from "./DBTest";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Router>
-          <div>
-            <ul>
-              <li>
-                <Link className="App-link" to="/">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link className="App-link" to="/about">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link className="App-link" to="/dashboard">
-                  Dashboard
-                </Link>
-              </li>
-            </ul>
-
-            <hr />
-
-            {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/about">
-                <About />
-              </Route>
-              <Route path="/dashboard">
-                <Dashboard />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      </header>
-    </div>
-  );
-}
-
-// You can think of these components as "pages"
-// in your app.
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
+      <Router>
+        <Container>
+          <Row>
+            <Col>
+              {" "}
+              <Nav>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/">
+                    Home
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/indexeddb-test">
+                    IndexedDB-Test
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Switch>
+                <Route exact path="/">
+                  <Home name="User" />
+                </Route>
+                <Route path="/indexeddb-test">
+                  <DBTest name="other User" />
+                </Route>
+              </Switch>
+            </Col>
+          </Row>
+        </Container>
+      </Router>
     </div>
   );
 }
---
+++ src/DBTest.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class DBTest extends React.Component {
+  render() {
+    return <h1>Here we are, {this.props.name}</h1>;
+  }
+}
